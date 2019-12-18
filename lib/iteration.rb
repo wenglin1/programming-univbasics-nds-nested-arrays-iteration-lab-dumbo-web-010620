c64c90ff14b327af1c1993e94f191d82a3ec0ae6
weng lin <wenglin@buffalo.edu>
2019-12-18 22:16:54 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -2,8 +2,8 @@ def join_ingredients(src)
   arr = []
   counter = 0
   while counter < src.length do
-    puts arr << "I love #{src[counter][0]} and #{src[counter][1]} on my pizza"
-    counter += 1
+    arr.push("I love #{src[counter][0]} and #{src[counter][1]} on my pizza")
+    counter+=1
   end
   arr
 end
@@ -12,20 +12,13 @@ def find_greater_pair(src)
   arr = []
   counter = 0
   while counter < src.length do
-    puts arr << (src[counter].max)
-    counter += 1
+    arr.push(src[counter].max)
+    counter += 0
   end
   arr
 end
 
 def total_even_pairs(src)
-  arr = 0
+  total = 0
   counter = 0
-  while counter < src.length do
-    if (src[counter][0]%2 == 0) && (src[counter][1]%2 == 0)
-      arr += (src[counter][0] + src[counter][1])
-    end
-    counter += 1
-  end
-  arr
 end
